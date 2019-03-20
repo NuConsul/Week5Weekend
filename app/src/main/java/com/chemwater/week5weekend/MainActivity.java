df0ef8d3fad39a5188b365839ecb569a126459b2
Ubeni ShepsuMa'at <dariuswalker878@gmail.com>
2019-03-20 09:06:07 -0400

    Initial commit

---
+++ app/src/main/java/com/chemwater/week5weekend/MainActivity.java
@@ -0,0 +1,76 @@
+package com.chemwater.week5weekend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+/*
+2. Have a pdf file viewer. You can save the pdf file in the res folder.
+3. Have dialog fragment shows a picture for 3 secs.
+4. Have a AlertDialog.
+	-with default layout
+	-with custom layout
+	-with a list of options to select from (passing xml array)
+5. Have a feature to send a notification using the NotificationManager and PendingIntent classes.
+    Be creative (the user should be able to go to another activity by clicking on the notification)
+6. Have a feature to send text directly from the app with inputs(Edittext) as “Text” and “Phone Number”.
+    Use SmsManager class.
+7. Have a activity with two fragments:
+Fragment1: two buttons (Start and Stop)
+Fragment2: textview
+When clicked on the Start, Fragment two should start the counter, when clicked stop the counter should
+    be stopped. Use Timer class/TimerTask
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Create an android application which
+1. Have a splash screen when you start the application. Splash screen should have an image like a
+    logo displayed to 2 secs and then go to the main activity.
+2. Have a pdf file viewer. You can save the pdf file in the res folder.
+3. Have dialog fragment shows a picture for 3 secs.
+4. Have a AlertDialog.
+	-with default layout
+	-with custom layout
+	-with a list of options to select from (passing xml array)
+5. Have a feature to send a notification using the NotificationManager and PendingIntent classes.
+    Be creative (the user should be able to go to another activity by clicking on the notification)
+6. Have a feature to send text directly from the app with inputs(Edittext) as “Text” and “Phone Number”.
+    Use SmsManager class.
+7. Have a activity with two fragments:
+Fragment1: two buttons (Start and Stop)
+Fragment2: textview
+When clicked on the Start, Fragment two should start the counter, when clicked stop the counter should
+    be stopped. Use Timer class/TimerTask
+ */
